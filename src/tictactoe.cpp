6beaeede0ae8a7fb4a1f5006d6c1e5ec7627938d
DPontes <dpontes@protonmail.com>
2019-07-02 19:50:27 +0200

    second commit, up to step 5 done

--- src/tictactoe.cpp
+++ src/tictactoe.cpp
@@ -1,9 +1,28 @@
 #include "tictactoe.hpp"
+#include <sstream>
 
 namespace tictactoe {
 
     bool always_true() {
         return true;
+    }
+
+    Position::Position(int x, int y) : _x{x}, _y{y} {}
+
+    std::string Position::to_string() const {
+        std::stringstream ss;
+        ss << "[" << _x << "," << _y << "]";
+        return ss.str();
+    }
+
+    std::string TicTacToe::to_string() const {
+        std::string board;
+
+        board += "---\n";
+        board += "---\n";
+        board += "---\n";
+
+        return board;
     }
 
 }
--- src/tictactoe.hpp
+++ src/tictactoe.hpp
@@ -1,8 +1,24 @@
 #ifndef TICTACTOE_HPP
 #define TICTACTOE_HPP
 
+#include <string>
+
 namespace tictactoe {
     bool always_true();
+
+    class Position {
+    public:
+        Position(int x, int y);
+        std::string to_string() const;
+        int _x;
+        int _y;
+    };
+
+    class TicTacToe {
+    public:
+        std::string to_string() const;
+    };
 }
+
 
 #endif //TICTACTOE_HPP
